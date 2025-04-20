35d75c30a59414504905c67479080a3e67b2240e
shreddykr <148612874+shreddykr@users.noreply.github.com>
2025-04-19 17:59:58 -0700

    Add files via upload

---
+++ skr-vegetation/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+description 'Lake Vinewood Added Vegetation w/optional Heli Pads'
+author 'shreddykr'
+version '1.1'
+
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    "optional/**/*",
+    "required/**/*",
+}
+
+dependencies {
+    'mbmaps',
+    'mbv_vol1',
+}
